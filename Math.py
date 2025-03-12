9e6210756b5fde592d31b22081382bdeeda61399
LilianWacuka <liliannguruw@gmail.com>
2025-03-12 18:41:14 +0300

    Assignment

---
+++ Math.py
@@ -0,0 +1,32 @@
+# Addition of two numbers
+num1=10
+num2=20
+sum=num1+num2
+print (sum)
+
+# Subtraction of two numbers
+num1=10
+num2 =5
+sub=num1-num2
+print (sub)
+
+# conditional statement
+num1=50
+num2=10
+if num1>num2:
+    print("num1 is greater than num2")
+else:
+    print("error")
+
+    #conditional statement2
+num1=50
+num2=100
+sub=num2-num1
+if num1>num2:
+    print("num1 is greater than num2")
+else:
+    print("error")
+
+    #conditional statement3
+
+
